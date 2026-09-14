23526534db68118036baf6f20a29087c57ed0e2c
DeletedArch <ahmedbasha243@gmail.com>
2026-09-15 00:07:18 +0300

    chore(wallhold): make wall hold slide slower and make dash vfx bigger

--- Assets/Animation/Player/WallHoldStateSMB.cs
+++ Assets/Animation/Player/WallHoldStateSMB.cs
@@ -2,11 +2,20 @@ using UnityEngine;
 
 public class WallHoldStateSMB : StateMachineBehaviour
 {
-    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    float originalGravityScale = 1f;
+    [SerializeField] float reductionFactor = 0.75f;
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        originalGravityScale = animator.GetComponent<Rigidbody2D>().gravityScale;
+        animator.GetComponent<Rigidbody2D>().gravityScale *= reductionFactor;
+
         int direction = animator.GetInteger("TouchingWall");
         animator.transform.localScale = new Vector3(-direction * Mathf.Abs(animator.transform.localScale.x), animator.transform.localScale.y, animator.transform.localScale.z);
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.GetComponent<Rigidbody2D>().gravityScale = originalGravityScale;
     }
 
 }
