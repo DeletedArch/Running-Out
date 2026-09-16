9f8d63a02d773da3e3c4c28bef5b3f3130ea7520
DeletedArch <ahmedbasha243@gmail.com>
2026-09-16 23:39:14 +0300

    feat(generation): Add level generation with difficulty scaling and cost calculation

--- Assets/Scripts/Data/Level/Area.cs
+++ Assets/Scripts/Data/Level/Area.cs
@@ -14,50 +14,20 @@ public enum AreaLevel
 public class Area : ScriptableObject
 {
     [SerializeField] private string areaName;
-    [SerializeField] private int areaID = Random.Range(0, 1000000);
+    [SerializeField] private int areaID;
     [Tooltip("Used to determine progression and region")]
     [SerializeField] private AreaLevel areaLevel;
     [SerializeField] private int enemyCount;
     [SerializeField] private GameObject areaPrefab;
-    [SerializeField] private Transform startingPoint;
-    [SerializeField] private Transform exitPoint;
 
     public string AreaName { get => areaName; }
     public int AreaID { get => areaID; }
     public AreaLevel AreaLevel { get => areaLevel; }
     public int EnemyCount { get => enemyCount; }
     public GameObject AreaPrefab { get => areaPrefab; }
-    public Transform StartingPoint { get => startingPoint; }
-    public Transform ExitPoint { get => exitPoint; }
-    [Header("Runtime")]
-    private GameObject areaInstance;
-    private bool isAreaInstantiated = false;
-    
-    public void InistantiateArea(Vector2 position)
-    {
-        if (areaPrefab != null && !isAreaInstantiated)
-        {
-            areaInstance = Instantiate(areaPrefab);
-            areaInstance.name = areaName;
-            areaInstance.transform.position = position; // Set the position to the origin or any desired position
-            isAreaInstantiated = true;
-        }
-        else
-        {
-            if (isAreaInstantiated)
-            {
-                Debug.LogWarning("Area is already instantiated: " + areaName);
-            }
-            else
-            {
-                Debug.LogWarning("Area prefab is not assigned for " + areaName);
-            }
-        }
-    }
 
-    public void DestroyArea()
+    void OnEnable()
     {
-        Destroy(areaInstance);
-        Destroy(this);
+        areaID = Random.Range(0, 1000000);
     }
 }
--- Assets/Scripts/Data/Level/Region.cs
+++ Assets/Scripts/Data/Level/Region.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
 using System;
 
-
+[CreateAssetMenu(fileName = "New Region", menuName = "LevelGeneration/Region")]
 public class Region : ScriptableObject
 {
     [SerializeField] private string regionName;
-    [SerializeField] private int regionID = UnityEngine.Random.Range(0, 1000000);
+    [SerializeField] private int regionID;
     [SerializeField] private AreaLevel regionLevel;
     [SerializeField] private Area[] availableAreas;
     [SerializeField, Tooltip("The starting point of the region, a new region means a new environment")] private int regionStart;
@@ -16,16 +16,33 @@ public class Region : ScriptableObject
     public AreaLevel RegionLevel { get => regionLevel; }
     public Area[] AvailableAreas { get => availableAreas; }
 
-    public Area GetRandomArea()
+    void OnEnable()
     {
+        regionID = UnityEngine.Random.Range(0, 1000000);
+    }
+
+    public Area GetRandomArea(int costLevel, out int remainingCost)
+    {
+        remainingCost = costLevel;
         if (availableAreas.Length > 0)
         {
-            int randomIndex = UnityEngine.Random.Range(0, availableAreas.Length);
-            return availableAreas[randomIndex];
+            Area[] filteredAreas = GetAreasByCostLevel(costLevel);
+            if (filteredAreas.Length > 0)
+            {
+                int randomIndex = UnityEngine.Random.Range(0, filteredAreas.Length);
+                remainingCost -= filteredAreas[randomIndex].EnemyCount;
+                return filteredAreas[randomIndex];
+            }
+            return null;
         }
         else
         {
             return null;
         }
+    }
+
+    Area[] GetAreasByCostLevel(int costLevel)
+    {
+        return Array.FindAll(availableAreas, area => area.EnemyCount <= costLevel);
     }
 }
